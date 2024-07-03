88878048fc58c18bd0364af783eafe885f736ccd
rserban <radu.serban@bertschi.com>
2024-07-03 10:08:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+const myName: string = "Radu";
+</script>
+
+<template>
+  <HelloWorld :user-name="myName" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Ref, ref, toRef } from 'vue'
+
+interface Props {
+  userName: string;
+}
+
+const props: Props = defineProps<Props>();
+const userName: Ref<string> = toRef(props, "userName");
+const count: Ref<number> = ref(0);
+
+</script>
+
+<template>
+  <h1>Hello {{ userName }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
